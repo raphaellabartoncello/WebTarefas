097640e4db2f5a4a07105aa15a12aee270ded35e
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-08 22:22:55 -0300

    Create TarefasMOD

---
+++ Impacta.Tarefas.Web/TarefasMOD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Impacta.Tarefas.Web
+{
+    public class TarefasMOD
+    {
+        public int Id { get; set; }
+        [Required] //Atributo que indica que o campo é obrigatório
+        [Display(Name ="Nome da Tarefa")]
+        [MaxLength(100, ErrorMessage ="Tamanho máximo de 100 caracteres")]
+        public string Nome { get; set; }
+        [Required]
+        public int Prioridade { get; set; }
+        [Display(Name = "Status Atual")]
+        public bool Concluida { get; set; }
+        [Display(Name = "Anotações")]
+        [MaxLength(200)]
+        public string Observacoes { get; set; }
+
+    }
+}
